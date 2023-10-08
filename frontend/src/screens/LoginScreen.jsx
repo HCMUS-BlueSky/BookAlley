923eafd3d4401c9c3239737f362060e94aab1732
Minh Khoi <nminhkhoi0818@gmail.com>
2023-10-08 10:21:29 +0700

    Handled Login and Register and added toast message

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,29 +1,98 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    let inputName = e.target.name;
+    if (inputName == "username") {
+      setUsername(value);
+    } else if (inputName == "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      if (!username || !password) {
+        toast.error("Invalid username, email or password!");
+      } else {
+        const user = {
+          // username: username,
+          email: "example@gmail.com",
+          password: password,
+        };
+        const response = await toast.promise(
+          axios.post("/api/auth/login", user),
+          {
+            pending: "Loading...",
+            success: "Login Successful!",
+            error: "Something went wrong!",
+          }
+        );
+        console.log(response);
+        if (response.status === 200) {
+          console.log("Successfully");
+          navigate("/home");
+        }
+        setUsername("");
+        setPassword("");
+      }
+    } catch (err) {
+      toast.error(err);
+    }
+  };
+
   return (
     <>
       <div className="main">
         <Header />
+        <ToastContainer />
         <section>
           <div className="login-card">
             <h2>Login</h2>
             <form action="">
               <label htmlFor=""></label>
-              <input type="text" placeholder="Username" />
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
               <label htmlFor=""></label>
-              <input type="text" placeholder="Password" />
-              <button type="button">
+              <input
+                type="text"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+              <button type="button" onClick={handleLogin}>
                 <p>Login</p>
               </button>
             </form>
             <div className="nav-btn">
               <button type="button">
-                <p>Forgot Password</p>
+                <Link to="/forgotpassword">
+                  <p>Forgot Password</p>
+                </Link>
               </button>
               <button type="button">
-                <p>Sign Up</p>
+                <Link to="/register">
+                  <p>Sign Up</p>
+                </Link>
               </button>
             </div>
           </div>
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,22 +1,96 @@
+import { useState } from "react";
+import axios from "axios";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 
 const RegisterScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    let inputName = e.target.name;
+    if (inputName == "username") {
+      setUsername(value);
+    } else if (inputName == "password") {
+      setPassword(value);
+    } else if (inputName == "confirmpassword") {
+      setConfirmPassword(value);
+    }
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      if (!username || !password || !confirmPassword) {
+        toast.error("Invalid username, email or password!");
+      } else if (password !== confirmPassword) {
+        toast.error(
+          "The confirmation password does not match. Please try again!"
+        );
+      } else if (password.length < 7) {
+        toast.error("Password too short");
+      } else {
+        const user = {
+          username: username,
+          password: password,
+          email: "example1@gmail.com",
+        };
+        const response = await toast.promise(
+          axios.post("/api/auth/register", user),
+          {
+            pending: "Loading...",
+            success: "Register Successful!",
+            error: "Something went wrong!",
+          }
+        );
+        setUsername("");
+        setPassword("");
+        setConfirmPassword("");
+      }
+    } catch (err) {
+      toast.error(err);
+    }
+  };
+
   return (
     <>
       <div className="main">
         <Header />
+        <ToastContainer />
         <section>
           <div className="login-card">
             <h2>Sign up</h2>
             <form action="">
-              <label htmlFor=""></label>
-              <input type="text" placeholder="Username" />
-              <label htmlFor=""></label>
-              <input type="text" placeholder="Password" />
-              <label htmlFor=""></label>
-              <input type="text" placeholder="Confirm password" />
-              <button type="button">
+              <label htmlFor="username"></label>
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+              <label htmlFor="password"></label>
+              <input
+                type="text"
+                value={password}
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+              />
+              <label htmlFor="confirmpassword"></label>
+              <input
+                type="text"
+                value={confirmPassword}
+                name="confirmpassword"
+                placeholder="Confirm password"
+                onChange={handleChange}
+              />
+              <button type="button" onClick={handleRegister}>
                 <p>Sign Up</p>
               </button>
             </form>
