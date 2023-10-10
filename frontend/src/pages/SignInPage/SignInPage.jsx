488791a747809f3db74aea9479924e11dcd30ab0
Minh Khoi <nminhkhoi0818@gmail.com>
2023-10-10 23:28:16 +0700

    Set up style, added style for Sign In Page

--- frontend/src/pages/SignInPage/SignInPage.jsx
+++ frontend/src/pages/SignInPage/SignInPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./SignInPage.scss";
 
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  return (
+    <div>
+      <div className="container">
+        <h1>Login</h1>
+        <form action="">
+          <label htmlFor="email">Email</label>
+          <input type="text" />
+          <label htmlFor="">Password</label>
+          <input type="text" />
+          <button type="button">Submit</button>
+        </form>
+        <p>
+          Don't have an account? Register<a href=""> now</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
