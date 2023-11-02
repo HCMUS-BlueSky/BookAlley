5f29f01a949953d6c24b74e4236f9d18cd3496bd
PSDat123 <sieudat123@gmail.com>
2023-11-02 14:39:52 +0700

    fix: Update proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://book-alley-api.onrender.com/api',
         changeOrigin: true,
         secure: false,
         ws: true
