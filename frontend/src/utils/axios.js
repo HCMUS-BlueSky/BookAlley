ba4112ec9d48eabe5bfc9f97b49609bf27ca550d
PSDat123 <sieudat123@gmail.com>
2023-11-02 21:04:13 +0700

    typo: typo in css & axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.baseURL || 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_baseURL || 'http://localhost:5000';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.baseURL || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_baseURL || 'http://localhost:5000',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
