b3b0d8d4ddc96beb372dbca440832c92341a37ff
Minh Khoi <nminhkhoi0818@gmail.com>
2023-11-04 21:14:49 +0700

    fix(seller): fix style add page

--- frontend/src/pages/SellerPage/AddProductPage.jsx
+++ frontend/src/pages/SellerPage/AddProductPage.jsx
@@ -26,54 +26,72 @@ const AddProductPage = () => {
           <div className="detail-card">
             <h2>2. Detail</h2>
             <hr />
-            <form action="">
-              <label htmlFor="">Author</label>
-              <input type="text" />
-            </form>
-            <form action="">
-              <label htmlFor="">Publisher</label>
-              <input type="text" />
-            </form>
-            <form action="">
-              <label htmlFor="">Translator</label>
-              <input type="text" />
-            </form>
-            <form action="">
-              <label htmlFor="">Page count</label>
-              <input type="number" />
-            </form>
-            <form action="">
-              <label htmlFor="">Publish date</label>
-              <input type="date" />
-            </form>
-            <form action="">
-              <label htmlFor="">Language</label>
-              <input type="text" />
-            </form>
+            <div className="form-group">
+              <form action="">
+                <label htmlFor="">Author</label>
+                <input type="text" />
+              </form>
+              <form action="">
+                <label htmlFor="">Publisher</label>
+                <input type="text" />
+              </form>
+              <form action="">
+                <label htmlFor="">Translator</label>
+                <input type="text" />
+              </form>
+              <form action="">
+                <label htmlFor="">Page count</label>
+                <input type="number" />
+              </form>
+              <form action="">
+                <label htmlFor="">Publish date</label>
+                <input type="date" />
+              </form>
+              <form action="">
+                <label htmlFor="">Language</label>
+                <input type="text" />
+              </form>
+            </div>
           </div>
           <div className="pricing-image-card">
             <h2>3. Pricing and Pictures</h2>
             <hr />
-            <label htmlFor="">Price</label>
-            <input type="number" />
-            <label htmlFor="">Product code</label>
-            <input type="text" />
-            <label htmlFor="">Weight</label>
-            <input type="number" />
-            <label htmlFor="">Dimensions</label>
-            <input type="text" />
-            <label htmlFor="">Product picture</label>
-            <input type="file" />
+            <div className="form-group">
+              <form action="">
+                <label htmlFor="">Price</label>
+                <input type="number" />
+              </form>
+              <form action="">
+                <label htmlFor="">Product code</label>
+                <input type="text" />
+              </form>
+              <form action="">
+                <label htmlFor="">Weight</label>
+                <input type="number" />
+              </form>
+              <form action="">
+                <label htmlFor="">Dimensions</label>
+                <input type="text" />
+              </form>
+              <form action="" className="file">
+                <label htmlFor="">Product picture</label>
+                <input type="file" />
+              </form>
+            </div>
           </div>
           <div className="description-card">
             <h2>4. Description</h2>
             <hr />
             <label htmlFor="">Product description</label>
-            <input type="text" />
+            <textarea name="" id="" cols="30" rows="10"></textarea>
           </div>
           <div className="nav-btn">
-            <button type="button">Cancel</button>
-            <button type="button">Submit</button>
+            <button type="button" className="btn-cancel">
+              Cancel
+            </button>
+            <button type="button" className="btn-submit">
+              Submit
+            </button>
           </div>
         </div>
       </div>
