c2ef1243dbfd3c38b7212c1e3f5b3bc6e5bb22b8
Uyen Tran <21127202@student.hcmus.edu.vn>
2024-01-04 22:18:03 +0700

    feat: process status order by seller

--- frontend/src/components/SellerTabs/PendingTabComponent.jsx
+++ frontend/src/components/SellerTabs/PendingTabComponent.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { processOrderSeller } from "../../actions/sellerAction";
 
 const PendingTabComponent = () => {
   const { orders } = useSelector((state) => state.seller);
+  const dispatch = useDispatch();
+
+  const handleProcess = (order_id, status) => {
+    dispatch(processOrderSeller({ order_id: order_id, status }));
+  };
+
+  useEffect(() => {}, [orders]);
   return (
     <>
       <table className="data-tab">
@@ -13,20 +21,42 @@ const PendingTabComponent = () => {
             <th>Payment method</th>
             <th>Total</th>
             <th>Status</th>
+            <th></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {orders.docs &&
             orders.docs.map((order) => {
-              return (
-                <tr key={order._id}>
-                  <td>{order._id}</td>
-                  <td>{order.shipping_method}</td>
-                  <td>{order.payment_method}</td>
-                  <td>{order.total && order.total.toLocaleString("en-US")}đ</td>
-                  <td>{order.status}</td>
-                </tr>
-              );
+              if (order.status == "pending") {
+                return (
+                  <tr key={order._id}>
+                    <td>{order._id}</td>
+                    <td>{order.shipping_method}</td>
+                    <td>{order.payment_method}</td>
+                    <td>
+                      {order.total && order.total.toLocaleString("en-US")}đ
+                    </td>
+                    <td>{order.status}</td>
+                    <td>
+                      <button
+                        className="process-btn"
+                        onClick={() => handleProcess(order._id, "delivering")}
+                      >
+                        Delivery
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="cancel-order-btn"
+                        onClick={() => handleProcess(order._id, "canceled")}
+                      >
+                        Cancel
+                      </button>
+                    </td>
+                  </tr>
+                );
+              }
             })}
         </tbody>
       </table>
