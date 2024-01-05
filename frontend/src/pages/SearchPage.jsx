d74a42f571b328a1aeda2ca403389abfc55d0e81
Faith <faithdanghuy@gmail.com>
2024-01-05 14:59:38 +0700

    main(product): product update

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,19 +1,57 @@
-import React, { useEffect } from "react";
-import { searchProduct } from "../actions/productsAction";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
 import HeaderComponent from "../components/HeaderComponent";
 import FooterComponent from "../components/FooterComponent";
+import { useSearchParams } from "react-router-dom";
+import { axiosPublicInstance } from "../utils/axios";
+import ProductsCardComponent from "../components/ProductCardComponent";
 
 const SearchPage = () => {
-  const searchParams = new URLSearchParams(document.location.search);
-  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    dispatch(searchProduct({ searchTerm: searchParams.toString() }));
+    async function fetchData() {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      let { data } = await axiosPublicInstance.get(
+        `api/book/search?searchTerm=${searchParams.get("q")}`,
+        config
+      );
+      setProducts(data.docs);
+    }
+    fetchData();
   }, []);
 
   return (
     <>
       <HeaderComponent />
+      <div
+        className="container-fluid"
+        style={{
+          backgroundColor: "#F0F0F0",
+          paddingTop: "0.5rem",
+          paddingBottom: "0.5rem",
+        }}
+      >
+        <div className="container search-items">
+          <div className="products">
+            <h2>Search Result</h2>
+            <div className="product-cards">
+              {products &&
+                products.map((product) => {
+                  return (
+                    <ProductsCardComponent
+                      key={product._id}
+                      product={product}
+                    />
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+      </div>
       <FooterComponent />
     </>
   );
