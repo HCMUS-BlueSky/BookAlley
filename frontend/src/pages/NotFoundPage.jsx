ffee4d07043b5a329c60d952933c9a210c906f1d
KitoKotowa <chaukiet220803@gmail.com>
2024-01-06 17:53:40 +0700

    Add 404 Not Found page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="auth">
+      <div className="container">
+        <h1>Oops!</h1>
+        <h2>404 - Not Found</h2>
+        <h3>Sorry, the page you requested was not found</h3>
+        <p>
+          <Link to="/signin">Return to sign in</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
