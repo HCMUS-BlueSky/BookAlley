f56972b78108a9eef6856a8e74d804d2c2f14b6d
Faith <faithdanghuy@gmail.com>
2024-01-08 15:49:38 +0700

    main(About Us): Add about us page

---
+++ frontend/src/pages/AboutUsPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutUsPage = () => {
+  return (
+    <div className="auth">
+      <div className="container">
+        <h1>Book Alley: Your Gateway to Literary Delights Online</h1>
+        <p>
+        Welcome to Book Alley, your premier destination for literary treasures! As a leading online book-selling platform, 
+        Book Alley is committed to providing book enthusiasts with a curated collection of diverse genres and bestsellers. 
+        With a user-friendly interface, seamless navigation, and secure transactions, we strive to create a delightful shopping experience. 
+        Explore our virtual alley of knowledge, where you can discover, purchase, and indulge in the joy of reading from the comfort of your own space. 
+        At Book Alley, we celebrate the love for literature, offering a haven for bibliophiles to embark on literary adventures with each click.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsPage;
